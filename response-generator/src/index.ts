1406d2844e2f3e93f20658cfc76072ff8fde38d9
pappijx <ayush.papani123@gmail.com>
2023-03-28 23:25:48 +0530

    add new changes for response generator

--- response-generator/src/index.ts
+++ response-generator/src/index.ts
@@ -1,20 +1,3 @@
-export const generateResponse = (props: any, requiredFields: string[]) => {
-  let resultantArray: any = {};
-  for (const key in props) {
-    if (typeof props[key] !== 'object') {
-      if (requiredFields.indexOf(key) !== -1) {
-        resultantArray[key] = props[key];
-      }
-    } else {
-      if (Array.isArray(props[key])) {
-        resultantArray[key] = props[key];
-      } else {
-        resultantArray = {
-          ...resultantArray,
-          ...generateResponse(props[key], requiredFields),
-        };
-      }
-    }
-  }
-  return resultantArray;
-};
+import generateResponse from './generateReponse';
+import arrayFormatter from './arrayFormatter';
+export { generateResponse, arrayFormatter };
